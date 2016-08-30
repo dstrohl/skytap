8053e3108eec1017d643a9762db8448fed94107c
strohl <Kur1tacams>
2016-08-30 12:38:16 -0700

    expanded convert_date to handle date time strings and spearate tz strings as is used in the shared portal section.

--- skytap/framework/Utils.py
+++ skytap/framework/Utils.py
@@ -1,11 +1,9 @@
 """Some basic utility functions that help out with the Skytap API."""
 
-from datetime import datetime
-from datetime import timedelta
-from datetime import tzinfo
+from datetime import datetime, timedelta, tzinfo, date, time
 import json
 import logging
-
+from skytap.framework.timezones import TIMEZONES
 
 try:  # Python 2.7+
     from logging import NullHandler
@@ -56,18 +54,72 @@ def error(err):
     return json.dumps({"error": err})
 
 
-def convert_date(date_str):
+def convert_date(date_str, tz_str=None):
+
     """Convert a Skytap date string to a datetime object.
+
+    will handle the following transformations:
+        will return Datetime objects:
+          - '2015/09/08 00:26:48 -0800'
+          - '2015/09/08 00:26:48', tz_str='-0800'
+          - '2015/09/08 00:26:48', tz_str='PST'
+          - '2015/09/08 00:26:48', tz_str='Pacific Standard Time'
+
+        will return Time objects:
+          - '00:26:48 -0800'
+          - '00:26:48', tz_str='-0800'
+          - '00:26:48', tz_str='PST'
+          - '00:26', tz_str='PST'
+          - '00:26:48', tz_str='Pacific Standard Time'
+
+        will return Date objects:
+          - '2015/09/08'
+            (note, this ignores any timezone information)
+
+        if no tz info is found, a naive object is returned.
 
     Sample from Skytap: 2015/09/08 00:26:48 -0800
     """
-    naive_date_str, _, offset_str = date_str.rpartition(' ')
-    naive_dt = datetime.strptime(naive_date_str, '%Y/%m/%d %H:%M:%S')
-    offset = int(offset_str[-4:-2]) * 60 + int(offset_str[-2:])
-    if offset_str[0] == "-":
-        offset = -offset
-    dt = naive_dt.replace(tzinfo=FixedOffset(offset))
-    return dt
+    parsed_date_obj = date_str.split(' ')
+    short_date_str = None
+    short_time_str = None
+    short_tz_str = None
+    has_tz_info = tz_str is not None
+    naive_ret = None
+    for pdo in parsed_date_obj:
+        if '/' in pdo:
+            short_date_str = pdo
+        elif ':' in pdo:
+            short_time_str = pdo
+        else:
+            short_tz_str = pdo
+            has_tz_info = True
+
+    if short_date_str is not None and short_time_str is not None:
+        short_dt_str = '%s %s' % (short_date_str, short_time_str)
+        naive_ret = datetime.strptime(short_dt_str, '%Y/%m/%d %H:%M:%S')
+
+    elif short_time_str is not None and short_date_str is None:
+        # date only
+        naive_ret = time(*':'.split(short_time_str))
+
+    if naive_ret is not None:
+        if has_tz_info:
+            if short_tz_str is not None:
+                offset_str = short_tz_str
+            else:
+                offset_str = TIMEZONES.get(tz_str, '-00:00')
+
+            offset = int(offset_str[-4:-2]) * 60 + int(offset_str[-2:])
+            if offset_str[0] == "-":
+                offset = -offset
+            dt = naive_ret.replace(tzinfo=FixedOffset(offset))
+            return dt
+        else:
+            return naive_ret
+    else:
+        naive_ret = date(*'/'.split(short_date_str))
+        return
 
 
 class FixedOffset(tzinfo):
